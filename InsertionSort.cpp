b4b8ad9cc6dfc4323efa49539b3a099fd642a845
Prashant Kumar <prasha3nt@gmail.com>
2022-11-13 11:59:35 +0530

    Create InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+void printArray(int A[], int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+}
+void Swap( int* x, int* y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+void InsertionSort(int A[], int n)
+{
+    
+    for(int i=1; i<n;i++)
+    {
+        int ToBeInserted=A[i];
+        int temp;
+        int j=i-1;
+        while(j>=0 && ToBeInserted<A[j])
+        {
+              A[j+1]=A[j];
+               j--;
+        }
+        A[j+1]=ToBeInserted;
+       
+    }
+}
+
+int main() {
+   
+int Array[]={6,5,4,3,2};
+
+int n=sizeof(Array)/sizeof(Array[0]);
+printArray(Array,n);
+InsertionSort(Array,n);
+printArray(Array, n);
+    return 0;
+}
